fb97e19935a83631b28e9b169d51dad1054afe6c
goldstargreat <goldstargreat@gmail.com>
2026-05-14 17:42:38 +0900

    완성

--- app/src/main/java/kr/ac/kopo/actionbarfragmenttest/MyTabFragment.java
+++ app/src/main/java/kr/ac/kopo/actionbarfragmenttest/MyTabFragment.java
@@ -13,49 +13,41 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class MyTabFragment extends Fragment{
+public class MyTabFragment extends Fragment {
     String tabName;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Bundle data = getArguments();
-        tabName = data.getString("tabName");
+        tabName = getArguments().getString("tabName");
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        LinearLayout baseLayout = new LinearLayout(super.getActivity());
+        LinearLayout baseLayout = new LinearLayout(getActivity());
         baseLayout.setOrientation(LinearLayout.VERTICAL);
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
-        baseLayout.setLayoutParams(params);
+        baseLayout.setLayoutParams(new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT));
         baseLayout.setGravity(Gravity.CENTER);
 
-        ImageView img1 = new ImageView(super.getActivity());
-        img1.setImageResource(R.drawable.dog);
+        ImageView img1 = new ImageView(getActivity());
         img1.setAdjustViewBounds(true);
-
         baseLayout.addView(img1);
 
         if (tabName.equals("Dog")) {
             baseLayout.setBackgroundColor(Color.GREEN);
             img1.setImageResource(R.drawable.dog);
         }
-        if (tabName.equals("Cat")){
+        if (tabName.equals("Cat")) {
             baseLayout.setBackgroundColor(Color.RED);
             img1.setImageResource(R.drawable.cat);
         }
-
-        if (tabName.equals("Horse")){
+        if (tabName.equals("Horse")) {
             baseLayout.setBackgroundColor(Color.rgb(204, 114, 61));
             img1.setImageResource(R.drawable.horse);
         }
-        if (tabName.equals("Rabbit")){
-            baseLayout.setBackgroundColor(Color.RED);
-            img1.setImageResource(R.drawable.rabbit);
-        }
-
 
         return baseLayout;
     }
